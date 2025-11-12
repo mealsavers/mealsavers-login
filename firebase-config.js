d1fefe0113f0ce07e3f24493eaba48194d273026
mealsavers <mealsavers24@gmail.com>
2025-11-11 18:36:13 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPB4C2DmnB193myunPKKwAwqKUnhzUeg0",
+  authDomain: "mealsavers-f5dbf.firebaseapp.com",
+  projectId: "mealsavers-f5dbf",
+  storageBucket: "mealsavers-f5dbf.appspot.com",
+  messagingSenderId: "254383045667",
+  appId: "1:254383045667:web:8c7c309e4661c78c8b8596",
+  measurementId: "G-CKMP2L4FB6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
